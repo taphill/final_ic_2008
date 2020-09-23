23c7e1279d0ab2da4b7e90e46e0638f7b6d61335
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 11:02:20 -0500

    Create ingredients method in CookBook class

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -8,4 +8,10 @@ class CookBook
   def add_recipe(recipe)
     recipes << recipe
   end
+
+  def ingredients
+    recipes.flat_map do |recipe|
+      recipe.ingredients.map(&:name)
+    end.uniq
+  end
 end
