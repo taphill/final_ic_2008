b715a36648fe59900c415cbdfd4141b83dc0a705
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 10:21:24 -0500

    Add tests for checking stock and restocking items

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -16,5 +16,30 @@ class PantryTest < Minitest::Test
     assert_equal ({}), pantry.stock
   end
 
-  
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
+
+  def test_it_can_restock
+    pantry = Pantry.new
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient2, 7)
+
+    assert_equal 7, pantry.stock_check(ingredient2)
+  end
 end
