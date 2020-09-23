9f7c719b7da5dc39003f2ab2fd32c5675d34b240
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 10:36:46 -0500

    Add test for returning all ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -37,6 +37,15 @@ class RecipeTest < Minitest::Test
   end
 
   def test_it_can_return_all_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
 
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
 end
