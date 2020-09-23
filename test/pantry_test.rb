530628d689ebf9ccdc85a5224379a3afef082150
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 10:11:32 -0500

    Add basic tests for Pantry class

---
+++ test/pantry_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_has_attributes
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  
+end
