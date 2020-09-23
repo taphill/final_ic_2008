8d7611ba7e0856e8664064b0daa1b9ae2bfd6a82
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 10:21:40 -0500

    Create stock_check and restock methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -2,6 +2,14 @@ class Pantry
   attr_reader :stock
 
   def initialize
-    @stock = {}
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(item)
+    stock[item]
+  end
+
+  def restock(item, quantity)
+    stock[item] += quantity
   end
 end
