3645892541dbad4fef0de2f60e4bfb98b0b50cdc
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 10:54:44 -0500

    Add test for total_calories method

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -48,4 +48,24 @@ class RecipeTest < Minitest::Test
 
     assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
+
+  def test_it_can_find_total_calories
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    assert_equal 440, recipe1.total_calories
+    assert_equal 675, recipe2.total_calories
+  end
 end
