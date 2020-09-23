2b988b36152925a75d1fcf8524638a8bd6684093
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-23 11:26:48 -0500

    Create enough_ingredients_for? method in Pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -12,4 +12,12 @@ class Pantry
   def restock(item, quantity)
     stock[item] += quantity
   end
+
+  def enough_ingredients_for?(recipe)
+    enough_ingredients = recipe.ingredients_required.map do |ingredient, amount|
+      stock[ingredient] >= amount
+    end
+
+    enough_ingredients.all?(true)
+  end
 end
